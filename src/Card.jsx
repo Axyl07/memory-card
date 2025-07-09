6440c022b72874965756f04416e2a6c06f634bb0
Axyl07 <aryanchauhan000777@gmail.com>
2025-07-09 14:49:20 +0530

    Added score functionality

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 
-export default function Card({ weapon }) {
 
+export default function Card({ weapon,handleScoreIncrement,handleScoreReset }) {
+
+    const [status, setStatus] = useState(false); 
+    function handleClick() {
+        console.log(weapon.name + " is clicked");
+        if (status == false) {
+            setStatus(true);
+            handleScoreIncrement();
+        } else {
+            handleScoreReset();
+            setStatus(false);
+        }
+    }
     return (
-        <div  className="card">
+        <div onClick={handleClick} className="card">
             <div className="cardImg">
                 <img src={weapon.img} alt={weapon.name} />
             </div>
             <div className="cardname">
-                { weapon.name }
+                {weapon.name}
             </div>
         </div>
     )
--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import Card from "./Card";
 export default function Grid({ weaponsList }) {
+    const [score, setScore] = useState(0);
+    // const [highScore, setHighScore] = useState(0);
+    function handleScoreIncrement() {
+        setScore((s) => s + 1);
+    }
+    function handleScoreReset() {
+        setScore(0);
+    }
     return (
+        <>
+        <div className="scoreboard">
+            Score : {score}
+        </div>
         <div className="grid" >
             { 
                 weaponsList.map((weapon) =>        
-                        <Card key={weapon.id + Math.random()} weapon={weapon} />
+                    <Card key={weapon.id} weapon={weapon} handleScoreIncrement={handleScoreIncrement} handleScoreReset={ handleScoreReset } />
                 )
             }
         </div>
+        </>
         
     )
 
